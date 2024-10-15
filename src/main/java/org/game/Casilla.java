9d1e7adad631fe9e9afd481740b297ba4d045924
meri <1392328@uab.cat>
2024-10-15 13:40:07 +0200

    Clase abstracta creada.

---
+++ src/main/java/org/game/Casilla.java
@@ -0,0 +1,20 @@
+package org.game;
+
+public abstract class Casilla {
+
+  public int fila;
+  public char columna;
+  boolean golpeada;
+
+  Casilla(int fila, char col)
+  {
+    this.fila = fila;
+    this.columna = col;
+    golpeada = false;
+  }
+
+  public abstract boolean esGolpeada();
+  public abstract void recibirGolpe();
+  public abstract void printCasilla();
+
+}
