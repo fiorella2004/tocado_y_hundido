5d448bb1a94e2cb6d3f5e4a65c34e26d2e951762
meri <1392328@uab.cat>
2024-10-15 18:03:56 +0200

    Clase Agua implementada.

--- src/main/java/org/game/Agua.java
+++ src/main/java/org/game/Agua.java
@@ -2,16 +2,37 @@ package org.game;
 
 public class Agua extends Casilla{
 
-  char aguaDibujo;
+  char aguaDibujo = '⬜';
 
-  Agua(int fila, char col) {super(fila, col);}
+  Agua(int fila, char col) {
+
+    super(fila, col);
+
+    if (fila < 1 || fila > 10) {
+      throw new IllegalArgumentException("La fila debe estar entre 1 y 10.");
+    }
+    if (col < 'A' || col > 'J') {
+      throw new IllegalArgumentException("La columna debe estar entre 'A' y 'J'.");
+    }
+
+  }
 
   @Override
-  public boolean esGolpeada() {return golpeada;}
+  public boolean esGolpeada()
+  {
+    return golpeada;
+  }
 
   @Override
-  public void recibirGolpe() {};
+  public void recibirGolpe()
+  {
+    golpeada = true;
+    aguaDibujo = '☒';
+  }
 
   @Override
-  public void printCasilla() {};
+  public void printCasilla()
+  {
+    System.out.print(aguaDibujo);
+  }
 }
