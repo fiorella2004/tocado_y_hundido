e4d94bab3424300452016c3dd06470e10d23e936
Fiorella Queirolo <FiorellaLucia.Queirolo@autonoma.cat>
2024-11-14 19:42:49 +0100

    clase Agua modificada logica y tests

--- src/main/java/org/game/Agua.java
+++ src/main/java/org/game/Agua.java
@@ -1,44 +1,20 @@
 package org.game;
 
-public class Agua extends Casilla{
+public class Agua extends Casilla {
 
-  protected char aguaDibujo = '⬜';
+  private String aguaDibujo = "🌊";
 
-  Agua(int fila, char col) {
-
-    super(fila, col);
-
-    if (fila < 1 || fila > 10) {
-      throw new IllegalArgumentException("La fila debe estar entre 1 y 10.");
-    }
-    if (col < 'A' || col > 'J') {
-      throw new IllegalArgumentException("La columna debe estar entre 'A' y 'J'.");
-    }
-
+  Agua(Coordenada coordenada) {
+    super(coordenada);
   }
 
   @Override
-  public char getDibujo()
-  {
+  public String getDibujo() {
     return aguaDibujo;
   }
 
   @Override
-  public boolean esGolpeada()
-  {
-    return golpeada;
-  }
+  public void recibirGolpe() {
 
-  @Override
-  public void recibirGolpe()
-  {
-    golpeada = true;
-    aguaDibujo = '☒';
-  }
-
-  @Override
-  public void printCasilla()
-  {
-    System.out.print(aguaDibujo);
   }
 }
--- src/test/java/org/game/AguaTest.java
+++ src/test/java/org/game/AguaTest.java
@@ -6,58 +6,23 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AguaTest {
 
   @Test
-  public void testConstructor_filaFueraDeRango_expectedException() {
-    // Arrange
-    Exception exception = assertThrows(IllegalArgumentException.class, () -> {new Agua(11, 'A');});
-
-    // Assert
-    assertEquals("La fila debe estar entre 1 y 10.", exception.getMessage());
-  }
-
-  @Test
-  public void testConstructor_columnaFueraDeRango_expectedException() {
-    // Arrange
-    Exception exception = assertThrows(IllegalArgumentException.class, () -> {new Agua(1, 'K');});
-
-    // Assert
-    assertEquals("La columna debe estar entre 'A' y 'J'.", exception.getMessage());
-  }
-
-  @Test
-  public void testConstructor_filaNegativa_expectedException() {
-    // Arrange
-    Exception exception = assertThrows(IllegalArgumentException.class, () -> {new Agua(-1, 'A');});
-
-    // Assert
-    assertEquals("La fila debe estar entre 1 y 10.", exception.getMessage());
-  }
-
-  @Test
-  public void testConstructor_columnaNegativa_expectedException() {
-    // Arrange
-    Exception exception = assertThrows(IllegalArgumentException.class, () -> {new Agua(1, '`');});
-
-    // Assert
-    assertEquals("La columna debe estar entre 'A' y 'J'.", exception.getMessage());
-  }
-
-  @Test
   public void testEsGolpeada_expectedFalse() {
     // Arrange
-    Agua agua = new Agua(1, 'A');
+    Coordenada coordenada = new Coordenada(1, 1);
+    Agua agua = new Agua(coordenada);
 
     // Act
     boolean golpeada = agua.esGolpeada();
 
     // Assert
     assertFalse(golpeada);
-    assertEquals(agua.getDibujo(), '⬜' );
   }
 
   @Test
   public void testRecibirGolpe_expectedTrue() {
     // Arrange
-    Agua agua = new Agua(1, 'A');
+    Coordenada coordenada = new Coordenada(2, 2);
+    Agua agua = new Agua(coordenada);
 
     // Act
     agua.recibirGolpe();
@@ -65,13 +30,13 @@ public class AguaTest {
 
     // Assert
     assertTrue(golpeada);
-    assertEquals(agua.getDibujo(), '☒' );
   }
 
   @Test
   public void testRecibirVariosGolpes_expectedTrue() {
     // Arrange
-    Agua agua = new Agua(1, 'A');
+    Coordenada coordenada = new Coordenada(3, 3);
+    Agua agua = new Agua(coordenada);
 
     // Act
     agua.recibirGolpe();
@@ -80,7 +45,6 @@ public class AguaTest {
 
     // Assert
     assertTrue(golpeada);
-    assertEquals(agua.getDibujo(), '☒' );
   }
 }
 
