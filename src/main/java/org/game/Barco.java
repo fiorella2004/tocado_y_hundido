ffc2a87b562effdfb114e81812821102f19b2801
meri <1392328@uab.cat>
2024-11-14 19:42:44 +0100

    Modificados test Barco y lógica

--- src/main/java/org/game/Barco.java
+++ src/main/java/org/game/Barco.java
@@ -2,36 +2,18 @@ package org.game;
 
 public class Barco extends Casilla {
 
-  protected char barcoDibujo = '⚪';
+  private String dibujo = "⛵";
 
-  Barco(int fila, char col) {
-    super(fila, col);
-    if (fila < 1 || fila > 10) {
-      throw new IllegalArgumentException("La fila debe estar entre 1 y 10.");
-    }
-    if (col < 'A' || col > 'J') {
-      throw new IllegalArgumentException("La columna debe estar entre 'A' y 'J'.");
-    }
+  public Barco(Coordenada coordenada) {
+    super(coordenada);
   }
 
   @Override
-  public boolean esGolpeada() {
-    return golpeada;
-  }
-
-  @Override
-  public char getDibujo() {
-    return barcoDibujo;
+  public String getDibujo() {
+    return dibujo;
   }
 
   @Override
   public void recibirGolpe() {
-    golpeada = true;
-    barcoDibujo = '⚫';
-  }
-
-  @Override
-  public void printCasilla() {
-    System.out.print(barcoDibujo);
   }
 }
