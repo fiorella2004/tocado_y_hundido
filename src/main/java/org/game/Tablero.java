d3f4009f4d69181bd8aa7bf99b2dc03acd2d6036
meri <1392328@uab.cat>
2024-11-15 17:56:05 +0100

    Clase implementada. Test failed 6, passed 4

--- src/main/java/org/game/Tablero.java
+++ src/main/java/org/game/Tablero.java
@@ -1,17 +1,55 @@
 package org.game;
 
+import java.util.ArrayList;
+
 public class Tablero {
   private ArrayList<Casilla> tablero = new ArrayList<>();
   private int idBarco = 1;
   private int numFilas;
   private int numCols;
 
-  public Tablero(int dimension){
+  public Tablero(int dimension) {
+
+    dimension = comprobarDimension(dimension);
+
+    this.numFilas = dimension;
+    this.numCols = dimension;
+
+
+    for (int fila = 0; fila < this.numFilas; fila++) {
+      for (int col = 0; col < this.numCols; col++) {
+        tablero.add(new Agua(new Coordenada(fila, col)));
+      }
+    }
   }
 
-  public void setNumFilas(int filas) {this.numFilas = filas;}
-  public void setNumCol(int cols) {this.numCols = cols;}
-  public int getNumFilas() {return numFilas;}
-  public int getNumCol() {return numCols;}
-  public ArrayList<Casilla> obtenerTablero() {return tablero;}
+  public int comprobarDimension(int dimension) {
+
+    int dimensionAux;
+
+    if (dimension < 10) {
+      dimensionAux = 10;
+      return dimensionAux;
+    } else if (dimension > 15) {
+      dimensionAux = 15;
+      return dimensionAux;
+    }
+
+    return dimension;
+  }
+
+  public void setDimension(int dimension) {
+  }
+
+  public int getNumFilas() {
+    return numFilas;
+  }
+
+  public int getNumCol() {
+    return numCols;
+  }
+
+  public ArrayList<Casilla> obtenerTablero() {
+    return tablero;
+  }
 }
