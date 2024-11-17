88ccb06f3a222ee3711216893da83887d071242c
Fiorella Lucia Queirolo Chávez <1666868@uab.cat>
2024-11-17 18:53:05 +0100

    clase jugador con constructor, test constructor

--- src/main/java/org/game/Jugador.java
+++ src/main/java/org/game/Jugador.java
@@ -1,5 +1,19 @@
 package org.game;
 
 public class Jugador {
+  private String nombre;
+  private Tablero tableroPrincipal;
+  private Tablero tableroSecundario;
 
+  public Jugador(String nombre, Tablero tableroPrincipal, Tablero tableroSecundario) {
+
+  }
+
+  public void asignarNombre(String nombre) {
+
+  }
+
+  public String getNombre() {
+
+  }
 }
--- src/test/java/org/game/JugadorTest.java
+++ src/test/java/org/game/JugadorTest.java
@@ -1,7 +1,23 @@
 package org.game;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JugadorTest {
+
+  @Test
+  void testAsignarNombre_expectedTrue() {
+    // Arrange
+    MockTablero mockTablero1 = new MockTablero(5);
+    MockTablero mockTablero2 = new MockTablero(5);
+    Jugador jugador = new Jugador("noname", mockTablero1, mockTablero2);
+
+    // Act
+    jugador.asignarNombre("Fiorella");
+
+    // Assert
+    assertEquals("Fiorella", jugador.getNombre());
+  }
+
 
 }
