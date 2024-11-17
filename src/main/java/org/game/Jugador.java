da021e9dd352b385c04058b93a2d730bd9584d22
Fiorella Lucia Queirolo Chávez <1666868@uab.cat>
2024-11-17 19:09:03 +0100

    implementacion de Jugador añadida, all test passed

--- src/main/java/org/game/Jugador.java
+++ src/main/java/org/game/Jugador.java
@@ -6,14 +6,16 @@ public class Jugador {
   private Tablero tableroSecundario;
 
   public Jugador(String nombre, Tablero tableroPrincipal, Tablero tableroSecundario) {
-
+    this.nombre = nombre;
+    this.tableroPrincipal = tableroPrincipal;
+    this.tableroSecundario = tableroSecundario;
   }
 
   public void asignarNombre(String nombre) {
-
+    this.nombre = nombre;
   }
 
   public String getNombre() {
-
+    return nombre;
   }
 }
--- src/test/java/org/game/JugadorTest.java
+++ src/test/java/org/game/JugadorTest.java
@@ -19,5 +19,4 @@ class JugadorTest {
     assertEquals("Fiorella", jugador.getNombre());
   }
 
-
 }
