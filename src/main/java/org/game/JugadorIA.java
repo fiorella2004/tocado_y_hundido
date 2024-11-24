02bdc5e06943f4d532930ad828b9b838110a54d5
meri <1392328@uab.cat>
2024-11-24 19:39:51 +0100

    Implementado método colocarBarco, generarCoordenadas y Direccion y calcularCoordenadas

--- src/main/java/org/game/JugadorIA.java
+++ src/main/java/org/game/JugadorIA.java
@@ -14,7 +14,17 @@ public class JugadorIA extends Jugador{
 
   @Override
   public boolean colocarBarco(ArrayList<Coordenada> casillasBarco, int dimensionBarco) {
-    return true;
+    boolean barcoColocado = false;
+    while (!barcoColocado) {
+      int fila = generarCoordenadaAleatoria();
+      int col = generarCoordenadaAleatoria();
+      int direccion = generarDireccionAleatoria();
+      ArrayList<Coordenada> coordenadas = calcularCoordenadasBarco(fila, col, direccion, dimensionBarco);
+      if (coordenadas != null) {
+        barcoColocado = tableroPrincipal.colocarBarco(coordenadas);
+      }
+    }
+    return barcoColocado;
   }
 
   @Override
@@ -25,6 +35,43 @@ public class JugadorIA extends Jugador{
   @Override
   public void registrarGolpe(Coordenada coordenada, Tablero tableroPrincipalOponente) {
 
+  }
+
+  private int generarCoordenadaAleatoria() {
+    Random random = new Random();
+    return random.nextInt(10); //0-9
+  }
+
+  private int generarDireccionAleatoria() {
+    Random random = new Random();
+    return random.nextInt(4) + 1;
+  }
+
+  private ArrayList<Coordenada> calcularCoordenadasBarco(int fila, int col, int direccion, int dimensionBarco) {
+    ArrayList<Coordenada> coordenadas = new ArrayList<>();
+    for (int i = 0; i < dimensionBarco; i++) {
+      int nuevaFila = fila;
+      int nuevaCol = col;
+      switch (direccion) {
+        case 1: // Arriba
+          nuevaFila = fila - i;
+          break;
+        case 2: // Abajo
+          nuevaFila = fila + i;
+          break;
+        case 3: // Izquierda
+          nuevaCol = col - i;
+          break;
+        case 4: // Derecha
+          nuevaCol = col + i;
+          break;
+      }
+      if (nuevaFila <= 0 || nuevaFila >= 10 || nuevaCol <= 0 || nuevaCol >= 10) {
+        return null;
+      }
+      coordenadas.add(new Coordenada(nuevaFila, nuevaCol));
+    }
+    return coordenadas;
   }
 }
 
