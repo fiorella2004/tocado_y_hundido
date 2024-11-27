53d419c19f65500930c018afe7143aeff59fc605
meri <1392328@uab.cat>
2024-11-27 20:36:14 +0100

    Creada rama VistaConsola

--- src/main/java/Vista/VistaConsola.java
+++ src/main/java/Vista/VistaConsola.java
@@ -1,4 +1,5 @@
 package Vista;
+
 import Modelo.*;
 
 import java.util.ArrayList;
@@ -6,33 +7,36 @@ import java.util.ArrayList;
 public class VistaConsola {
   private Partida partida;
 
-  public VistaConsola(){};
+  public VistaConsola() {
+  }
 
-  public void mostrarMensajeInicioJuego(){
+  ;
+
+  public void mostrarMensajeInicioJuego() {
 
   }
 
-  public void mostrarMensajeFinJuego(){
+  public void mostrarMensajeFinJuego() {
 
   }
 
-  public void mostrarMensajeTurnoIA(){
+  public void mostrarMensajeTurnoIA() {
 
   }
 
-  public String pedirGolpe(){
+  public String pedirGolpe() {
     return "0";
   }
 
-  public int pedirDimensionTablero(){
+  public int pedirDimensionTablero() {
     return 10;
   }
 
-  public void setPartida(Partida partida){
+  public void setPartida(Partida partida) {
     this.partida = partida;
   }
 
-  public void mostrarTableros(){
+  public void mostrarTableros() {
   }
 
   public void mostrarErrorColocarBarco() {
