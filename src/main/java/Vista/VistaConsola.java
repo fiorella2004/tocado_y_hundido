9bfd7c6aa25b07a2ff0b208439660a9bdbd51cc4
meri <1392328@uab.cat>
2024-11-27 21:54:51 +0100

    Implementados métodos en clase VistaConsola

--- src/main/java/Vista/VistaConsola.java
+++ src/main/java/Vista/VistaConsola.java
@@ -3,47 +3,122 @@ package Vista;
 import Modelo.*;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class VistaConsola {
+  private Scanner scanner = new Scanner(System.in);
   private Partida partida;
 
   public VistaConsola() {
-  }
-
-  ;
-
-  public void mostrarMensajeInicioJuego() {
-
-  }
-
-  public void mostrarMensajeFinJuego() {
-
-  }
-
-  public void mostrarMensajeTurnoIA() {
-
-  }
-
-  public String pedirGolpe() {
-    return "0";
-  }
-
-  public int pedirDimensionTablero() {
-    return 10;
   }
 
   public void setPartida(Partida partida) {
     this.partida = partida;
   }
 
-  public void mostrarTableros() {
+
+  public void mostrarMensajeInicioJuego() {
+    System.out.println("Bienvenido/a al juego de Tocado y Hundido");
+    System.out.println();
+  }
+
+  public void mostrarMensajeFinJuego() {
+    System.out.println("Fin del juego");
+    System.out.println();
+  }
+
+  public void mostrarMensajeTurnoIA() {
+    System.out.println("Turno del oponente");
+    System.out.println();
+  }
+
+  public int pedirDimensionTablero() {
+    System.out.println("Indica qué medida de tablero deseas:\n");
+    System.out.println("Opción 1: 10x10\n");
+    System.out.println("Opción 2: 11x11\n");
+    System.out.println("Opción 3: 12x12\n");
+    System.out.println("Opción 4: 13x13\n");
+    System.out.println("Opción 5: 14x14\n");
+    System.out.println("Opción 6: 15x15\n");
+
+    int opcionSeleccionada;
+
+    while (true) {
+      if (scanner.hasNextInt()) {
+        opcionSeleccionada = scanner.nextInt();
+
+        if (opcionSeleccionada >= 1 && opcionSeleccionada <= 6) {
+          return convertirDimensionTablero(opcionSeleccionada);
+        } else {
+          System.out.println("Por favor, selecciona un número entre 1 y 6\n");
+        }
+      } else {
+        System.out.println("Entrada inválida. Introduce un número\n");
+        scanner.next();
+      }
+    }
+  }
+
+  public int convertirDimensionTablero(int opcionSeleccionada){
+    switch (opcionSeleccionada) {
+      case 1:
+        return 10;
+      case 2:
+        return 11;
+      case 3:
+        return 12;
+      case 4:
+        return 13;
+      case 5:
+        return 14;
+      case 6:
+        return 15;
+    }
+    return -1;
+  }
+
+
+  public String pedirColocarBarco(int dimensionBarco) {
+    switch (dimensionBarco) {
+      case 2:
+        return pedirColocarBarco2casilla();
+      case 3:
+        return pedirColocarBarco3casilla();
+      case 4:
+        return pedirColocarBarco4casilla();
+      case 5:
+        return pedirColocarBarco5casilla();
+    }
+    return "null";
+  }
+
+  public String pedirColocarBarco2casilla() {
+    System.out.println("Introduce las coordenadas del barco de 2 casillas (ejemplo: 1A 2A): ");
+    return scanner.nextLine();
+  }
+
+  public String pedirColocarBarco3casilla(){
+    System.out.println("Introduce las coordenadas del barco de 3 casillas (ejemplo: 1A 2A 3A): ");
+    return scanner.nextLine();
+  }
+
+  public String pedirColocarBarco4casilla(){
+    System.out.println("Introduce las coordenadas del barco de 4 casillas (ejemplo: 1A 2A 3A 4A): ");
+    return scanner.nextLine();
+  }
+
+  public String pedirColocarBarco5casilla(){
+    System.out.println("Introduce las coordenadas del barco de 5 casillas (ejemplo: 1A 2A 3A 4A 5A):");
+    return scanner.nextLine();
+  }
+
+
+  public String pedirGolpe() {
+    System.out.println("Introduce las coordenadas donde quieras lanzar un golpe (ejemplo: 1A):");
+    return scanner.nextLine();
   }
 
   public void mostrarErrorColocarBarco() {
-
-  }
-
-  public String pedirColocarBarco(int dimensionBarco) {
-    return null;
+    System.out.println("Las coordenadas no se han introducido en el formato correcto\n");
   }
 }
