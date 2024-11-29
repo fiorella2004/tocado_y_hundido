1e0d83132b8bf2f7b456fd07541efbb66f771a40
Fiorella Lucia Queirolo Chávez <1666868@uab.cat>
2024-11-29 19:37:57 +0100

    prueba CI/CD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,11 +7,13 @@ import Vista.VistaConsola;
 public class Main {
 
   public static void main(String[] args) {
+    // Creando objetos para el juego
     VistaConsola vista = new VistaConsola();
     JugadorPersona jugadorPersona = new JugadorPersona("Persona");
     JugadorIA jugadorIA = new JugadorIA("IA");
     Partida partida = new Partida(jugadorPersona, jugadorIA);
     Controlador controlador = new Controlador(vista, partida, jugadorPersona, jugadorIA);
+    // Iniciar el juego
     controlador.colocarBarcosJugadorPersona();
     controlador.comenzarPartida();
   }
